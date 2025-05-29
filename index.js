66ec0b71926e0d345b1146f83a8033245551e30e
MUKUL <mukul.2226csit1115@kiet.edu>
2025-05-29 08:10:58 +0530

    Chnages done

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 
 // MongoDB connection
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/course-management', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://aggarwalmukul2194:course123@coursemanagment.yl0tfki.mongodb.net/', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
